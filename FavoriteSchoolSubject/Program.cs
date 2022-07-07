369780ab828dcab290933f56d806e763919bcd36
cpheighter <cheighterjr@gmail.com>
2022-07-07 12:48:34 -0400

    init

---
+++ FavoriteSchoolSubject/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("What was your favorite subject in school? ");
+var favSubject = Console.ReadLine();
+
+switch(favSubject)
+{
+    case "Math":
+        Console.WriteLine("Your favorite subject in school was Math");
+        break;
+
+    case "English":
+    case "ELA":
+        Console.WriteLine("Your favorite subject in school was English/ELA");
+        break;
+
+    case "Gym":
+        Console.WriteLine("Your favorite subject in school was Gym");
+        break;
+
+    case "Science":
+        Console.WriteLine("Your favorite subject in school was Science");
+        break;
+
+    case "Foregin Language":
+    case "Spanish":
+    case "French":
+        Console.WriteLine("Your favorite subject in school was a Foregin Language");
+        break;
+
+    default:
+        Console.WriteLine($"Your favorite subject in school was {favSubject}");
+        break;
+}
